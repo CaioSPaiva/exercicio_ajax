f6444ddd1116f472c3c1bd3ab4ef5ecd7bedd742
CaioSPaiva <caiopaiva134@gmail.com>
2024-11-05 19:22:57 -0300

    ajustes de js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
+
     const nameElement = document.querySelector('#name');
     const usernameElement = document.querySelector('#username');
     const avatarElement = document.querySelector('#avatar');
@@ -7,20 +8,25 @@ document.addEventListener('DOMContentLoaded', function() {
     const followingElement = document.querySelector('#following');
     const linkElement = document.querySelector('#link');
 
-    fetch('https://github.com/CaioSPaiva')
-        .then(function(res) {
-            return res.json();
-        })
-        .then(function(json) {
-            nameElement.innerText = json.name;
-            usernameElement.innerText = json.login;
-            avatarElement.src = json.avatar_url;
-            followingElement.innerText = json.following;
-            followersElement.innerText = json.followers;
-            reposElement.innerText = json.public_repos;
-            linkElement.href = json.html_url;
-        })
-        .catch(function(erro) {
-            alert("Um erro inesperado aconteceu no carregamento, tente novamente mais tarde.");
-        })
-})
+try {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', 'https://github.com/CaioSPaiva', true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const data = JSON.parse(xhr.responseText);
+            if (nameElement) nameElement.innerText = data.name;
+            if (usernameElement) usernameElement.innerText = data.login;
+            if (avatarElement) avatarElement.src = data.avatar_url;
+            if (followingElement) followingElement.innerText = data.following;
+            if (followersElement) followersElement.innerText = data.followers;
+            if (reposElement) reposElement.innerText = data.public_repos;
+            if (linkElement) linkElement.href = data.html_url;
+        } else if (xhr.readyState === 4) {
+            console.error('Error fetching data:', xhr.statusText);
+        }
+    };
+    xhr.send();
+} catch (error) {
+    console.error('Error fetching data:', error.message);
+}
+});
